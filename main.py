c755f9df5eb85b0cf68b08f7c1501d222b9b1570
halakhodary11-png <halakhodary11@gmail.com>
2026-06-16 16:12:56 +0300

    Create main.py
    Added first version of the game

---
+++ main.py
@@ -0,0 +1,13 @@
+print("🏺 Escape the Pyramid 🏺")
+print("You are trapped inside an ancient pyramid!")
+
+name = input("What is your name, explorer? ")
+
+print("Welcome", name)
+
+answer = input("You see a locked door. What is 2 + 2? ")
+
+if answer == "4":
+    print("The door opens! 🚪")
+else:
+    print("Wrong answer! The door stays closed.")
